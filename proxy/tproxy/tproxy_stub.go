e4144cd42bc39c39d6c853f3f2b4857966e38abb
qtopie <qtopie.rw@gmail.com>
2026-05-23 22:15:30 +0800

    fix err in macos

--- proxy/tproxy/tproxy_stub.go
+++ proxy/tproxy/tproxy_stub.go
@@ -11,3 +11,15 @@ import (
 func GetOriginalDst(conn net.Conn) (string, error) {
 	return "", fmt.Errorf("tproxy not supported on this platform")
 }
+
+func ListenUDPTransparent(port int) (*net.UDPConn, error) {
+	return nil, fmt.Errorf("udp transparent proxy not supported on this platform")
+}
+
+func DialUDPTransparent(origDst *net.UDPAddr) (*net.UDPConn, error) {
+	return nil, fmt.Errorf("udp transparent proxy not supported on this platform")
+}
+
+func ReadFromUDPWithOrigDst(conn *net.UDPConn, b []byte, oob []byte) (n int, src *net.UDPAddr, dst *net.UDPAddr, err error) {
+	return 0, nil, nil, fmt.Errorf("udp transparent proxy not supported on this platform")
+}
